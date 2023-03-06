eb22d777e681dc6e36f0f8f54e55b6455044542b
Mark Holdsworth <43139034+Holdsy@users.noreply.github.com>
2023-03-06 09:16:51 +0000

    Initial Commit

---
+++ LearningApp/LearningAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningAppApp.swift
+//  LearningApp
+//
+//  Created by Mark Holdsworth on 06/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct LearningAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
