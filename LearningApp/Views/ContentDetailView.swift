1b072a919dd988e397ff9c475cec7e6988dc7ed1
Mark Holdsworth <43139034+Holdsy@users.noreply.github.com>
2023-03-07 21:41:56 +0000

    add reusable rectangle card background add current content selected property to the content model add tag

--- LearningApp/Views/ContentDetailView.swift
+++ LearningApp/Views/ContentDetailView.swift
@@ -39,13 +39,33 @@ struct ContentDetailView: View {
                     
                     ZStack {
                     
-                        Rectangle()
+                        RectangleCard(color: Color.green)
                             .frame(height:48)
-                            .foregroundColor(Color.green)
-                            .cornerRadius(10)
-                            .shadow(radius:5)
+                         
                         
                         Text("Next Lesson: \(model.currentModule!.content.lessons[model.currentLessonIndex + 1].title)")
+                            .foregroundColor(Color.white)
+                            .bold()
+                    }
+                })
+            }
+            else {
+         // Show the completed button insted
+                
+                Button(action: {
+                    
+                    // Take the user back to the home view
+                    model.currentContentSelected = nil
+                    
+                }, label: {
+                    
+                    ZStack {
+                    
+                        RectangleCard(color: Color.green)
+                            .frame(height:48)
+                          
+                        
+                        Text("Complete")
                             .foregroundColor(Color.white)
                             .bold()
                     }
