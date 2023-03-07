d0984d8b0f820fc8e986940338ae8b6d1b4164dc
Mark Holdsworth <43139034+Holdsy@users.noreply.github.com>
2023-03-07 13:53:25 +0000

    - add video player - add content detail view

---
+++ LearningApp/Resources/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  LearningApp
+//
+//  Created by Mark Holdsworth on 07/03/2023.
+//
+
+import Foundation
+
+struct Constants {
+    
+    static var videoHostUrl = "https://codewithchris.github.io/learningJSON/"
+    
+}
