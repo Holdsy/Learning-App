048013f79b2a49038eb702d2941aedbf0578b561
Mark Holdsworth <43139034+Holdsy@users.noreply.github.com>
2023-03-10 09:04:04 +0000

    - Add code to process network code in background

--- LearningApp/ViewModels/ContentModel.swift
+++ LearningApp/ViewModels/ContentModel.swift
@@ -120,9 +120,13 @@ class ContentModel: ObservableObject {
                     
                     // Decode
                     let modules = try decoder.decode([Module].self, from: data!)
-                    
-                    // Append parsed modules into modules property
-                    self.modules += modules
+                   
+                    // Background thread
+                    DispatchQueue.main.async {
+                        // Append parsed modules into modules property
+                        self.modules += modules 
+                    }
+                   
                 }
                 catch {
                     // Couldn't parse json    
